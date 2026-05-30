8f8de0bb78866f541821f6668b710a1eb150f954
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-05-30 21:35:20 +0200

    test: make test runner wrapper lua friendly

--- scripts/run_tests.lua
+++ scripts/run_tests.lua
@@ -1,7 +1,13 @@
 --[[
 Purpose: Simple wrapper to run all CreateRailNet-V3 tests.
-Usage: shell.run("scripts/run_tests.lua") or dofile("scripts/run_tests.lua")
+Usage: shell.run("scripts/run_tests.lua") or dofile("scripts/run_tests.lua") or lua scripts/run_tests.lua
 ]]
+
+package.path = table.concat({
+  "./?.lua",
+  "./?/init.lua",
+  package.path or ""
+}, ";")
 
 pcall(dofile, "tests/harness/cc_bootstrap.lua")
 dofile("tests/harness/runner.lua")
